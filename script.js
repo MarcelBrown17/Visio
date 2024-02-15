007daf38b8be3b73e1cdf7493c9677ab027817c6
MarcelBrown17 <marcelbrown413.com>
2024-02-15 12:15:14 +0200

    Done

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+// $(document).ready(function () {
+//     $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+//     $('.next').on('click', function () {
+//         var currentImg = $('.active');
+//         var nextImg = currentImg.next();
+//         if (nextImg.length) {
+//             currentImg.removeClass().css('z-index', -10);
+//             nextImg.addClass('active').css('z-index', 10);
+//         } else {
+//             $('.slider-inner img').removeClass().css('z-index', -10);
+//             $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+//         }
+//     });
+// });
+
 $(document).ready(function () {
     $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+    $('.dot:first-child').addClass('active');
+    
     $('.next').on('click', function () {
-        var currentImg = $('.active');
-        var nextImg = currentImg.next();
-        if (nextImg.length) {
-            currentImg.removeClass().css('z-index', -10);
-            nextImg.addClass('active').css('z-index', 10);
-        } else {
-            $('.slider-inner img').removeClass().css('z-index', -10);
-            $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
-        }
+        nextSlide();
+    });
+    
+    $('.dot').on('click', function () {
+        var index = $(this).index();
+        $('.active').removeClass('active').css('z-index', -10);
+        $('.slider-inner img').eq(index).addClass('active').css('z-index', 10);
+        $('.dot.active').removeClass('active');
+        $(this).addClass('active');
     });
 });
+
+function nextSlide() {
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
+    if (nextImg.length) {
+        currentImg.removeClass('active').css('z-index', -10);
+        nextImg.addClass('active').css('z-index', 10);
+    } else {
+        $('.slider-inner img').removeClass('active').css('z-index', -10);
+        $('.slider-inner img:first-child').addClass('active').css('z-index', 10);
+    }
+    var currentIndex = $('.active').index('.slider-inner img');
+    $('.dot.active').removeClass('active');
+    $('.dot').eq(currentIndex).addClass('active');
+}
+
